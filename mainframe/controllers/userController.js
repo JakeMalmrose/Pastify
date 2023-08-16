f9d50ec8ed6ef023d94966a9c0573b2387cb59c9
JakeMalmrose <jmalmrose@student.neumont.edu>
2023-08-15 18:44:33 -0600

    Initial commit for usercontroller

---
+++ mainframe/controllers/userController.js
@@ -0,0 +1,34 @@
+const { User } = require('../models');
+
+const userController = {
+    register: async (req, res) => {
+        try {
+            const user = await User.create({
+                Username: req.body.username,
+                Password: req.body.password,
+                Email: req.body.email
+            })
+            res.status(200).json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    authenticate: async (req, res) => {
+        try {
+            const user = await User.findOne({where: { Email: req.body.email }});
+            if (!user) {
+                res.status(404).json({message: "User not found"});
+            }
+            match = await user.checkPassword(req.body.password); // authentication here
+            if (match) {
+                res.status(200).json(user);
+            } else {
+                res.status(401).json({message: "Incorrect password"});
+            }
+        } catch (err) {
+            res.status(500).json("Error during authentication:" + err);
+        }
+    }
+}
+
+module.exports = userController;
