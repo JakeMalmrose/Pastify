3f0b6b8f603c8c675c66d7db1955d8b5da985b98
Christian McCulley <127411172+chowder65@users.noreply.github.com>
2023-08-24 18:29:35 -0600

    made a function to search by songname and improved the funciton to find songs by artist and returns all songs

--- mainframe/controllers/yt_api.js
+++ mainframe/controllers/yt_api.js
@@ -1,18 +1,33 @@
 const YTMusic = require("ytmusic-api").default
 
-//test code used for the ytAPi that will search for songs 
 const ytmusic = new YTMusic()
 const ytMusicFunction = {
-    getSongUrl: async (req, res) => {
-    try {
-        await ytmusic.initialize();
-        const searchResult = await ytmusic.searchSongs(req.body.artistname);
-        const videoId = searchResult[0].videoId;
-        const stringWithoutSpaces = "youtube.com/watch?v=" + videoId.replace(/\s+/g, '');
-        res.status(200).json({ "url": stringWithoutSpaces });
-        } catch (error) {
-        res.status(500).json({ "error": 'An error occurred' });
-        }
+    getSongsByArtist: async (req, res) => {
+            try {
+                await ytmusic.initialize();
+                const artistID = await ytmusic.searchArtists(req.body.artistname)
+                const searchResult = await ytmusic.getArtistSongs(artistID[0].artistId);
+                let songList = {};
+                for (let i = 0; i < searchResult.length; ++i) {
+                    const videoId = searchResult[i].videoId;
+                    const stringWithoutSpaces = "youtube.com/watch?v=" + videoId.replace(/\s+/g, '');
+                    songList[searchResult[i].name] = stringWithoutSpaces;
+                }
+                return res.status(200).json(songList);
+            } catch (error) {
+                res.status(500).json({ "error": 'An error occurred' });
+            }
+    },
+    getSongByName: async (req, res) =>{
+        try {
+            await ytmusic.initialize();
+            const searchResult = await ytmusic.searchSongs(req.body.songname);
+            const videoId = searchResult[0].videoId;
+            const stringWithoutSpaces = "youtube.com/watch?v=" + videoId.replace(/\s+/g, '');
+            res.status(200).json({ "url": stringWithoutSpaces });
+            } catch (error) {
+            res.status(500).json({ "error": 'An error occurred' });
+            }
     }
     };
     
--- mainframe/routes/yt_api.js
+++ mainframe/routes/yt_api.js
@@ -2,6 +2,7 @@ const express =  require('express')
 const router = express.Router()
 const ytApiController = require('../controllers/yt_api')
 
-router.post('/getSongByArtist', ytApiController.getSongUrl)
+router.post('/getSongByArtist', ytApiController.getSongsByArtist)
+router.post('/getSongByName', ytApiController.getSongByName)
 
 module.exports = router
